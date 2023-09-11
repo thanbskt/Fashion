181e85b9ad6e646c9c2d92c5e985ffdb6b5ac4c5
thanbsk <athlyras@gmail.com>
2023-09-11 14:24:59 +0300

    updating classification file

--- gradio_classification.py
+++ gradio_classification.py
@@ -10,7 +10,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import load_img 
 from tensorflow.keras.applications.xception import preprocess_input
 
-model = load_model('/home/thanos/Desktop/Sustainable_fashion/repos/Fashion/model_class_polyvore_vong_v1_NO_prob.h5')
+model = load_model('/notebooks/model_class_polyvore_vong_v1_NO_prob.h5')
 
 
 class_names={0: 'Accessories',
@@ -35,14 +35,19 @@ def get_classification(path_example):
     result2 = 'Second Class name: {}, class number: {}\n'.format(class_names[predicted_classes[1]],predicted_classes[1])
     # Return the dominant image
     return resutl1,result2
-
-title = "Classifications for cloth images"
-description = "Takes an image as input and the category of the cloth"
+title = "Classifications for cloth images on VONG dataset version 1"
+description = "Takes an image as input and the category of the cloth \n NOTE: Images should have the fashion item in a white background for beter results"
 inputs = [gr.Image(type='filepath')]
 outputs = [gr.Textbox(label='First Prediction'),gr.Textbox(label='Second Prediction')]
 allow_flagging = 'manual'
 flagging_options = ['First prediction is wrong']
 
-demo = gr.Interface(fn=get_classification,inputs=inputs,outputs=outputs, allow_flagging=allow_flagging,flagging_options=flagging_options )
-demo.launch()
+demo = gr.Interface(fn=get_classification,
+                    inputs=inputs,
+                    outputs=outputs,
+                    description=description,
+                    title=title,
+                    allow_flagging=allow_flagging,
+                    flagging_options=flagging_options )
+demo.launch(share=True)
 
